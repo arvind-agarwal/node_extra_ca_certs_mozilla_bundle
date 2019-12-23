33560994eeddae4f1ccaf896ba845321d39f62d2
Arvind Agarwal <arvindsagarwal@gmail.com>
2019-12-23 13:45:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const csvtojson = require('csvtojson');
+const fs = require('fs');
+const request = require('request-promise-native');
+const bundleFileName = 'dist/mozilla_root_intermediate_bundle.pem';
+
+
+async function appendPEMFromUrl(url) {
+    console.log(`Downloading from URL ${url}`);
+    const data = await request.get(url);
+    console.log('Parsing data');
+    const jsonData = await csvtojson().fromString(data);
+
+    for (const entry of jsonData) {
+        // Retrieve PEM Info column and remove quotes in beginning and end
+        const certPem = entry['PEM Info'].slice(1, -1);
+        const commonName = entry['Common Name or Certificate Name'] || entry['Certificate Subject Common Name'];
+        const serialNumber = entry['Certificate Serial Number'];
+
+        // Add common Name and serial number
+        console.log(`Processing ${commonName} - ${serialNumber}`);
+        fs.appendFileSync(bundleFileName, `${commonName} - ${serialNumber}\n${certPem}\n\n`);
+    }
+    console.log(`URL ${url} processed\n\n`);
+}
+
+async function build() {
+    // clear file if exists
+    fs.writeFileSync(bundleFileName, '');
+    await appendPEMFromUrl('https://ccadb-public.secure.force.com/mozilla/PublicAllIntermediateCertsWithPEMCSV');
+    await appendPEMFromUrl('https://ccadb-public.secure.force.com/mozilla/IncludedCACertificateReportPEMCSV');
+}
+
+build();
